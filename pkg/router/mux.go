030c86cac83b850cd52408df4449f4e951ae57a6
GIO Systems <57343754+gio-systems@users.noreply.github.com>
2020-12-25 12:52:03 +0100

    remove unused funcs from router package

--- pkg/router/mux.go
+++ pkg/router/mux.go
@@ -7,24 +7,12 @@ import (
 
 //Mux wrapper for gorilla/mux
 type Mux struct {
-	Router *mux.Router
+	*mux.Router
 }
 
 //New return Mux with a standard mux Router
 func New() *Mux {
 	return &Mux{Router: mux.NewRouter()}
-}
-
-func (m *Mux) Handle(pattern string, handler http.Handler) {
-	m.Router.Handle(pattern, handler)
-}
-
-func (m *Mux) Method(method, pattern string, handler http.Handler) {
-	m.Router.Handle(pattern, handler).Methods(method)
-}
-
-func (m *Mux) HandleFunc(pattern string, handleFunc http.HandlerFunc) {
-	m.Router.HandleFunc(pattern, handleFunc)
 }
 
 func (m *Mux) UseMiddleware(middlewares func(http.Handler) http.Handler) {
