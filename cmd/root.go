6892ec79e7173262bd3301950ddbd28436008eb7
bitcubix <bitcubix@protonmail.ch>
2021-04-14 15:25:56 +0200

    wrap cobra

--- cmd/root.go
+++ cmd/root.go
@@ -1,41 +1,20 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+	"github.com/bitcubix/golang-rest-api/pkg/cli"
 )
 
-var cfgFile string
-
-var rootCmd = &cobra.Command{
-	Use:   "golang-rest-api",
-	Short: "RESTful API Boilerplate written in GO",
-}
+var rootCmd = cli.NewCommand(
+	"golang-rest-api",
+	"RESTful API Boilerplate written in GO",
+	"",
+)
 
 func Execute() {
-	cobra.CheckErr(rootCmd.Execute())
+	cli.CheckErr(rootCmd.Execute())
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is ./config.yaml)")
+	// TODO rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is ./config.yaml)")
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	} else {
-		viper.AddConfigPath("./")
-		viper.SetConfigName(".config")
-	}
-
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
 }
